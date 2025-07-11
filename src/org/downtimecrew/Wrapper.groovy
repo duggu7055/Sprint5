de1ecedd98927b186c857e9beb325286e0da98fd
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-11 14:17:31 +0530

    Update Wrapper.groovy

--- src/org/downtimecrew/Wrapper.groovy
+++ src/org/downtimecrew/Wrapper.groovy
@@ -1,6 +1,5 @@
 package org.downtimecrew
 
-
 class Wrapper implements Serializable {
     def steps
 
@@ -9,35 +8,35 @@ class Wrapper implements Serializable {
     }
 
     def init(String dir = 'terraform') {
-        steps.echo "🔧 Running: terraform init in ${dir}"
+        steps.echo "Running: terraform init in ${dir}"
         steps.dir(dir) {
             steps.sh 'terraform init'
         }
     }
 
     def validate(String dir = 'terraform') {
-        steps.echo "🔧 Running: terraform validate in ${dir}"
+        steps.echo "Running: terraform validate in ${dir}"
         steps.dir(dir) {
             steps.sh 'terraform validate'
         }
     }
 
     def plan(String dir = 'terraform', String varfile = 'terraform.tfvars') {
-        steps.echo "🔧 Running: terraform plan in ${dir} with ${varfile}"
+        steps.echo "Running: terraform plan in ${dir} with ${varfile}"
         steps.dir(dir) {
             steps.sh "terraform plan -var-file=${varfile}"
         }
     }
 
     def applyTerraform(String dir = 'terraform', String varfile = 'terraform.tfvars') {
-        steps.echo "🔧 Running: terraform apply in ${dir} with ${varfile}"
+        steps.echo "Running: terraform apply in ${dir} with ${varfile}"
         steps.dir(dir) {
             steps.sh "terraform apply -auto-approve -var-file=${varfile}"
         }
     }
 
     def destroy(String dir = 'terraform', String varfile = 'terraform.tfvars') {
-        steps.echo "🔧 Running: terraform destroy in ${dir} with ${varfile}"
+        steps.echo "Running: terraform destroy in ${dir} with ${varfile}"
         steps.dir(dir) {
             steps.sh "terraform destroy -auto-approve -var-file=${varfile}"
         }
